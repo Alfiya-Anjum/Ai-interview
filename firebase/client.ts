03176901d2af93c8fcb6a912b0e8237fff9095b2
Alfiya-Anjum <125040235+Alfiya-Anjum@users.noreply.github.com>
2025-06-18 20:40:18 +0300

    ignore eslint and errors

--- firebase/client.ts
+++ firebase/client.ts
@@ -9,7 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-
+  apiKey: "AIzaSyBnmd87Fd0q1ijSG6d1ThqA7FXck0yWYdw",
+  authDomain: "ai-interview-9e92b.firebaseapp.com",
+  projectId: "ai-interview-9e92b",
+  storageBucket: "ai-interview-9e92b.firebasestorage.app",
+  messagingSenderId: "352536317239",
+  appId: "1:352536317239:web:ef7cd1d5fb6bbcc6ed5eb2",
+  measurementId: "G-KRV3MD3QW8"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
